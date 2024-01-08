fc4dfd7630ced81e241924fcb12f087dc9d36ae1
SupriyaBudhathoki <supriya.budhathoki98@gmail.com>
2024-01-08 13:17:42 +0545

    changes

--- Mathl.java
+++ Mathl.java
@@ -3,6 +3,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Mathl extends Remote {
+    
     //int add(int x, int y) throws RemoteException;
     int subtract(int x, int y) throws RemoteException;
     int multiply(int x, int y) throws RemoteException;
